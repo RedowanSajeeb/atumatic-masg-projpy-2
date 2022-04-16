769b09b13ed0b3ddb434a155f02ec3d31a38e621
Redowan Sajeeb <96831440+redowan-sajeeb@users.noreply.github.com>
2022-04-16 15:22:31 +0600

    first commit

---
+++ sms.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+message = 10
+while message > 0 :
+    time.sleep(1)
+    pyautogui.typewrite('hey-hablul-dodBAba')
+    time.sleep(1)
+    message = message - 1
+    pyautogui.press('enter')
